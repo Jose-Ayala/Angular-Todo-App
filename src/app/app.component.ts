9c6b5e8706d1ed3ba0cd79d24a11a12c4b0d1f1d
Jose Ayala <jayala4145@gmail.com>
2021-02-14 20:46:47 -0500

    Todo app initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray: string[] = [];
+
+  todoSubmit(value: any){
+    if (value!==""){
+      this.todoArray.push(value.todo)
+    } else {
+      alert('Field required**')
+    }
+  }
+
+  addTodo(value: string) {
+    this.todoArray.push(value);
+  }
+
+  deleteItem(todo: string) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
 }
